bceb5f5b729ebf7a87153bf325255f5c84aeb869
heikkilotjonen <heikkimlotjonen@gmail.com>
2025-11-04 18:43:10 +0200

    Pylint sääntöjen rikkomiset korjattu kunnolla

--- src/varasto.py
+++ src/varasto.py
@@ -1,19 +1,11 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
+        self.saldo = 0.0 if alku_saldo < 0.0 else min(alku_saldo, self.tilavuus)
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
 
     # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
